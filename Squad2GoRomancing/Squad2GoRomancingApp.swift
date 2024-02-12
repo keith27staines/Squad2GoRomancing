4566fb8b955e09a6289fbe2cda88a6a9ae4577dd
keith27staines <keith27staines@icloud.com>
2024-02-12 09:38:33 +0000

    Initial Commit

---
+++ Squad2GoRomancing/Squad2GoRomancingApp.swift
@@ -0,0 +1,17 @@
+//
+//  Squad2GoRomancingApp.swift
+//  Squad2GoRomancing
+//
+//  Created by Keith Staines on 12/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct Squad2GoRomancingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
